1fa9ee39a286a1b320cdbafce22a88aefeb5acf5
Yvan Razafindramanana <mail@ludeo.net>
2023-11-07 22:28:13 +0000

    fix(api): directions debug

---
+++ api/AzureMapsDirections.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+namespace suno;
+
+public class AzureMapsSummary
+{
+    public decimal lengthInMeters { get; set; }
+    public int travelTimeInSeconds { get; set; }
+    public int trafficDelayInSeconds { get; set; }
+    public decimal trafficLengthInMeters { get; set; }
+    public DateTimeOffset departureTime { get; set; }
+    public DateTimeOffset arrivalTime { get; set; }
+}
+
+public class AzureMapsPoint
+{
+    public decimal latitude { get; set; }
+    public decimal longitude { get; set; }
+}
+
+public class AzureMapsLeg
+{
+    public AzureMapsSummary summary { get; set; } = new();
+    public ICollection<AzureMapsPoint> points { get; set; } = Array.Empty<AzureMapsPoint>();
+}
+
+public class AzureMapsSection
+{
+    public int startPointIndex { get; set; }
+    public int endPointIndex { get; set; }
+    public string? sectionType { get; set; }
+    public string? travelMode { get; set; }
+}
+
+public class AzureMapsGuidanceInstruction
+{
+    public decimal? routeOffsetInMeters { get; set; }
+    public int travelTimeInSeconds { get; set; }
+    public AzureMapsPoint point { get; set; } = new();
+    public int pointIndex { get; set; }
+    public string? instructionType { get; set; }
+    public string? street { get; set; }
+    public bool possibleCombineWithNext { get; set; }
+    public string? drivingSide { get; set; }
+    public string? junctionType { get; set; }
+    public decimal? turnAngleInDecimalDegrees { get; set; }
+    public string? maneuver { get; set; }
+}
+
+public class AzureMapsGuidance
+{
+    public ICollection<AzureMapsGuidanceInstruction> instructions { get; set; } = Array.Empty<AzureMapsGuidanceInstruction>();
+}
+
+public class AzureMapsRoute
+{
+    public AzureMapsSummary summary { get; set; } = new();
+    public ICollection<AzureMapsLeg> legs { get; set; } = Array.Empty<AzureMapsLeg>();
+    public ICollection<AzureMapsSection> sections { get; set; } = Array.Empty<AzureMapsSection>();
+    public AzureMapsGuidance guidance { get; set; } = new();
+}
+
+public class AzureMapsDirectionsResponse
+{
+    public string? formatVersion { get; set; }
+    public ICollection<AzureMapsRoute> routes { get; set; } = Array.Empty<AzureMapsRoute>();
+}
--- api/Directions.cs
+++ api/Directions.cs
@@ -1,59 +1,116 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Net.Http;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace suno;
 
 public static class Directions
 {
+	private static readonly string azureMapsApiKey = Environment.GetEnvironmentVariable("AZURE_MAPS_API_KEY") ?? throw new Exception("AZURE_MAPS_API_KEY not set");
+
+	private static readonly HttpClient httpClient = new();
+
+	private static readonly JsonSerializerOptions jsonSerializerOptions = new() { Converters = { new JsonStringEnumConverter() } };
+
 	[FunctionName("Directions")]
-	public static IActionResult Run(
+	public static async Task<IActionResult> Run(
 		[HttpTrigger(AuthorizationLevel.Function, "get", Route = "directions")] HttpRequest req,
 		ILogger log)
 	{
-		return new OkObjectResult(new { legs });
+		string fromLatitude = req.Query["fa"];
+		string fromLongitude = req.Query["fo"];
+		string toLatitude = req.Query["ta"];
+		string toLongitude = req.Query["to"];
+		string startDate = req.Query["d"];
+
+		if (!IsValidCoordinate(fromLatitude, fromLongitude, toLatitude, toLongitude) || !IsValidStartDate(startDate))
+		{
+			return new BadRequestResult();
+		}
+
+		if ("now".Equals(startDate, StringComparison.InvariantCulture))
+		{
+			startDate = "";
+		}
+
+		string azureMapsApiEndpoint = "https://atlas.microsoft.com/route/directions/json";
+
+		string requestUrl = $"{azureMapsApiEndpoint}?" +
+			$"api-version=1.0&subscription-key={azureMapsApiKey}&" +
+			$"query={fromLatitude},{fromLongitude}:{toLatitude},{toLongitude}&" +
+			$"departAt={startDate}&"
+			// TODO instead of legs use guidance : instructionsType=coded
+			;
+
+		var response = await httpClient.GetAsync(requestUrl);
+
+		if (!response.IsSuccessStatusCode)
+		{
+			log.LogError($"Azure Maps API request failed with status code: {response.StatusCode}");
+			return new StatusCodeResult(StatusCodes.Status502BadGateway);
+		}
+
+		var jsonContent = await response.Content.ReadAsStreamAsync();
+
+		var azureMapsResponse = await JsonSerializer.DeserializeAsync<AzureMapsDirectionsResponse>(jsonContent, jsonSerializerOptions);
+
+		if (azureMapsResponse?.routes.Count is not > 0)
+		{
+			return new NotFoundObjectResult("Route not found.");
+		}
+
+		var legs = azureMapsResponse.routes
+				.First().legs
+				.Select(MapLeg)
+				.ToList();
+
+		var trip = new Trip
+		{
+			legs = legs
+		};
+
+		return new OkObjectResult(trip);
 	}
 
-	private static Leg[] legs = new[] {
-			new Leg() {
-				start = new () {
-					name = "Nantes",
-					coord = new() {
-						lat = 47.2292m,
-						lon = -1.547m,
-					}
-				},
-				end = new() {
-					name = "Angers",
-					coord = new() {
-						lat = 47.4736m,
-						lon = -0.5548m,
-					}
-				},
-				durationInSeconds = 1 * 60 * 60
-			},
-			new() {
-				start = new() {
-					name = "Angers",
-					coord = new() {
-						lat = 47.4736m,
-						lon = -0.5548m,
-					}
-				},
-				end = new() {
-					name = "Paris",
-					coord = new() {
-						lat = 48.8566m,
-						lon = 2.3511m,
-					}
-				},
-				durationInSeconds = (int)(3.5 * 60 * 60)
-			},
-	};
+	private static bool IsValidCoordinate(params string[] coordinates)
+	{
+		return coordinates.All(IsAnumber);
+
+		static bool IsAnumber(string maybeAnumber) => decimal.TryParse(maybeAnumber, out _);
+	}
+
+	private static bool IsValidStartDate(string startDate) =>
+		"now".Equals(startDate, StringComparison.InvariantCulture)
+		|| DateTimeOffset.TryParse(startDate, out _);
+
+	internal static Leg MapLeg(AzureMapsLeg azureMapsLeg)
+	{
+		return new Leg
+		{
+			start = MapCoordinates(azureMapsLeg.points.First()),
+			end = MapCoordinates(azureMapsLeg.points.Last()),
+			durationInSeconds = azureMapsLeg.summary.travelTimeInSeconds
+		};
+	}
+
+	internal static Location MapCoordinates(AzureMapsPoint azureMapsPoint)
+	{
+		return new Location
+		{
+			name = "?",
+			coord = new Coordinates
+			{
+				lat = azureMapsPoint.latitude,
+				lon = azureMapsPoint.longitude
+			}
+		};
+	}
 }
