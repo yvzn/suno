10ff765fe368a1597ebd2d0ffe4204a17afdf17c
Yvan Razafindramanana <mail@ludeo.net>
2023-11-08 16:47:46 +0000

    feat(app): hide app name from screens

--- app/src/components/AppTitle.jsx
+++ app/src/components/AppTitle.jsx
@@ -3,26 +3,38 @@ import { useEffect, useRef } from "preact/hooks";
 
 export function AppTitle(props) {
     const headingRef = useRef();
+    const docTitleRef = useRef();
 
     useEffect(() => {
         if (!headingRef.current) return;
-
         headingRef.current.focus();
-        document.title = headingRef.current.innerText;
     }, [headingRef.current])
 
+    useEffect(() => {
+        if (!docTitleRef.current) return;
+        docTitleRef.current.style.display = 'none';
+        document.title = docTitleRef.current.innerText;
+    }, [docTitleRef.current])
+
     return (
-        <h1 ref={headingRef} tabIndex={-1}>
-            {props.title && (
-                <>
+        <>
+            <h1 ref={headingRef} tabIndex={-1}>
+                {props.title || (
                     <Text id="app.title">Suno</Text>
-                    {' : '}
-                    {props.title}
-                </>
-            )}
-            {!props.title && (
-                <Text id="app.title">Suno</Text>
-            )}
-        </h1>
+                )}
+            </h1>
+            <h2 ref={docTitleRef} aria-hidden="true">
+                {props.title && (
+                    <>
+                        <Text id="app.title">Suno</Text>
+                        {' : '}
+                        {props.title}
+                    </>
+                )}
+                {!props.title && (
+                    <Text id="app.title">Suno</Text>
+                )}
+            </h2>
+        </>
     );
 }
